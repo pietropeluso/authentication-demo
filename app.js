b8925f22d7d77e79c064b331d854bf0e9a2c840e
Pietro Peluso <pietro.peluso@gmail.com>
2015-05-17 11:54:00 +0100

    Fixing layout issue in index.jade and adding /dashboard route

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require("express");
 
 var app = express();
 
+// all environments
 app.set("view engine", "jade");
 app.set("port", process.env.PORT || 1337);
 
+app.locals.pretty = true; // disable html minifying by Jade
+
+// Routes
 app.get("/", function(req, res){
   res.render("index.jade");
 });
@@ -15,6 +19,14 @@ app.get("/register", function(req, res){
 
 app.get("/login", function(req, res){
   res.render("login.jade");
+});
+
+app.get("/dashboard", function(req, res){
+  res.render("dashboard.jade");
+});
+
+app.get("/logout", function(req, res){
+  res.redirect("/");
 });
 
 app.listen(app.get('port'), function(){
