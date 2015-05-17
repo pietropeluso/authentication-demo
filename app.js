5b99613af9f7ab9970f765f90e9c86b23fcfa443
Pietro Peluso <pietro.peluso@gmail.com>
2015-05-17 11:30:11 +0100

    Committing the app.js file with the main index view

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "jade");
+app.set("port", process.env.PORT || 1337);
+
+app.get("/", function(req, res){
+  res.render("index.jade");
+});
+
+app.listen(app.get('port'), function(){
+  console.log("Web server listening on: 127.0.0.1:" + app.get('port'));
+})
